daa01e9c2d38ddb7ef96f000c8802a62a4cadb5e
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2026-05-01 22:36:48 -0400

    create hw

---
+++ src/js/render-functions.js
@@ -0,0 +1,189 @@
+// Описаний у документації
+// import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.js-container');
+const loader = document.querySelector('.loader');
+
+// Ініціалізуємо SimpleLightbox для всіх зображень у галереї
+const lightbox = new SimpleLightbox('.js-container .image-card a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+});
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.style.display = 'block';
+}
+
+export function hideLoader() {
+  loader.style.display = 'none';
+}
+
+
+
+
+export function renderFoto(data) {
+
+    const fragment = document.createDocumentFragment();
+
+    data.hits.forEach(image => {
+        // Створюємо контейнер для карточки зображення
+        const card = document.createElement('li');
+        card.classList.add('image-card');
+
+        // Створюємо контейнер для обгортки посилання та інформації
+        const container = document.createElement('div');
+        container.classList.add('li-cont');
+
+        // Створюємо посилання
+        const link = document.createElement('a');
+        link.href = image.largeImageURL;
+        link.classList.add('link-a')
+
+        // Створюємо зображення
+        const imgElement = document.createElement('img');
+        imgElement.classList.add('pic-card');
+        imgElement.src = image.webformatURL;
+        imgElement.alt = image.tags;
+
+        // Додаємо зображення до посилання
+        link.appendChild(imgElement);
+
+        // Додаємо посилання з зображенням до контейнера
+        container.appendChild(link);
+
+        // Створюємо елемент div для обгортки інформації
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('item-text');
+
+        // Створюємо список ul
+        const infoList = document.createElement('ul');
+        infoList.classList.add('image-info');
+
+        // Створюємо елементи списку li для інформації про зображення
+        const likesInfo = document.createElement('li');
+        likesInfo.textContent = `Likes: ${image.likes}`;
+
+        const viewsInfo = document.createElement('li');
+        viewsInfo.textContent = `Views: ${image.views}`;
+
+        const commentsInfo = document.createElement('li');
+        commentsInfo.textContent = `Comments: ${image.comments}`;
+
+        const downloadsInfo = document.createElement('li');
+        downloadsInfo.textContent = `Downloads: ${image.downloads}`;
+
+        // Додаємо елементи li до списку ul
+        infoList.appendChild(likesInfo);
+        infoList.appendChild(viewsInfo);
+        infoList.appendChild(commentsInfo);
+        infoList.appendChild(downloadsInfo);
+
+        // Додаємо список ul до елемента div
+        infoContainer.appendChild(infoList);
+
+        // Додаємо контейнер зі списком до карточки
+        container.appendChild(infoContainer);
+
+        // Додаємо контейнер до фрагменту
+        card.appendChild(container);
+
+        // Додаємо карточку до галереї
+        fragment.appendChild(card);
+    });
+
+    // Додаємо фрагмент з карточками до галереї
+    gallery.appendChild(fragment);
+
+    lightbox.refresh();
+}
+
+
+// Функція для додавання додаткових зображень у галерею
+
+export function renderMorePhotos(data) {
+    const gallery = document.querySelector('.js-container');
+    const fragment = document.createDocumentFragment();
+
+    data.hits.forEach(image => {
+        // Створення DOM-елементів карточки тут
+        const card = document.createElement('li');
+        card.classList.add('image-card');
+
+        // Створюємо контейнер для обгортки посилання та інформації
+        const container = document.createElement('div');
+        container.classList.add('li-cont');
+
+        // Створюємо посилання
+        const link = document.createElement('a');
+        link.href = image.largeImageURL;
+         link.classList.add('link-a')
+
+        // Створюємо зображення
+        const imgElement = document.createElement('img');
+        imgElement.classList.add('pic-card');
+        imgElement.src = image.webformatURL;
+        imgElement.alt = image.tags;
+
+        // Додаємо зображення до посилання
+        link.appendChild(imgElement);
+
+        // Додаємо посилання з зображенням до карточки
+        container.appendChild(link);
+
+        // Створюємо елемент div для обгортки інформації
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('item-text');
+
+        // Створюємо список ul
+        const infoList = document.createElement('ul');
+        infoList.classList.add('image-info');
+
+        // Створюємо елементи списку li для інформації про зображення
+        const likesInfo = document.createElement('li');
+        likesInfo.textContent = `Likes: ${image.likes}`;
+
+        const viewsInfo = document.createElement('li');
+        viewsInfo.textContent = `Views: ${image.views}`;
+
+        const commentsInfo = document.createElement('li');
+        commentsInfo.textContent = `Comments: ${image.comments}`;
+
+        const downloadsInfo = document.createElement('li');
+        downloadsInfo.textContent = `Downloads: ${image.downloads}`;
+
+        // Додаємо елементи li до списку ul
+        infoList.appendChild(likesInfo);
+        infoList.appendChild(viewsInfo);
+        infoList.appendChild(commentsInfo);
+        infoList.appendChild(downloadsInfo);
+
+        // Додаємо список ul до елемента div
+        infoContainer.appendChild(infoList);
+
+        // Додаємо контейнер зі списком до карточки
+        container.appendChild(infoContainer);
+
+        // Додаємо контейнер до карточки
+        card.appendChild(container);
+
+        // Додаємо карточку до фрагменту
+        fragment.appendChild(card);
+    });
+
+    // Додаємо фрагмент з карточками до галереї
+    gallery.appendChild(fragment);
+
+    // Ініціалізуємо SimpleLightbox для всіх зображень у галереї
+    // const lightbox = new SimpleLightbox('.js-container .image-card a', {
+    //      captionsData: 'alt',
+    //      captionDelay: 250,
+    // });
+    lightbox.refresh();
+}
---
+++ src/main.js
@@ -0,0 +1,106 @@
+import { searchFoto } from './js/pixabay-api.js';
+import { renderFoto, renderMorePhotos,  clearGallery, showLoader, hideLoader  } from './js/render-functions.js';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.js-container');
+const loader = document.querySelector('.loader');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+let currentPage = 1;
+let currentQuery = '';
+let totalHits = 0;
+
+
+// Додаємо обробник події click для кнопки
+form.addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const query = document.getElementById('search-input').value.trim();
+    if (!query) {
+        iziToast.warning({
+            title: 'Warning',
+            message: 'Please enter a search images',
+        });
+        return;
+    }
+
+    // Показати завантажувач після кліку на кнопку
+  showLoader();
+    // Сховати кнопку
+    loadMoreBtn.style.display = 'none';
+    currentPage = 1;
+    currentQuery = query;
+
+    try {
+        const data = await searchFoto(query, currentPage);
+        if (data.hits.length === 0) {
+            iziToast.error({
+                title: 'Error',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+            });
+        } else {
+            totalHits = data.totalHits;
+            // Очистити попередній рендер перед новим
+            gallery.innerHTML = '';
+
+            renderFoto(data);
+
+            if (data.hits.length >= 15) {
+                loadMoreBtn.style.display = 'block';
+            }
+
+            // Очистити значення поля вводу
+            document.getElementById('search-input').value = '';
+
+        }
+    } catch (error) {
+        if (gallery.innerHTML === '') {
+            iziToast.error({
+                title: 'Error',
+                message: 'An error occurred while searching for images',
+            });
+        }
+    } finally {
+      hideLoader();
+    }
+});
+
+loadMoreBtn.addEventListener('click', async function(event) {
+    event.preventDefault();
+    loader.style.display = 'block';
+
+    try {
+        currentPage++;
+        const data = await searchFoto(currentQuery, currentPage);
+        if (data.hits.length > 0) {
+            const cardHeight = document.querySelector('.image-card').getBoundingClientRect().height;
+            renderMorePhotos(data);
+            window.scrollBy({
+                top: cardHeight * 2, // Прокручуємо на дві висоти карточки галереї
+                behavior: 'smooth' // Додаємо плавність
+            });
+        }
+    } catch (error) {
+        // Якщо немає більше зображень для завантаження
+        loadMoreBtn.style.display = 'none';
+        iziToast.warning({
+            title: 'Warning',
+            message: 'No more images to load',
+        });
+    } finally {
+        if (gallery.querySelectorAll('.image-card').length >= totalHits) {
+            loadMoreBtn.style.display = 'none';
+            iziToast.info({
+                title: 'Info',
+                message: "We're sorry, but you've reached the end of search results.",
+            });
+        }
+      hideLoader();
+    }
+});
+
+
